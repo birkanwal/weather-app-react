2773531aab772c85c24a0e82f34b86663bfb4947
birkanwal <80430034+birkanwal@users.noreply.github.com>
2021-07-16 17:34:48 -0400

    Integrated api

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <h1>Weather Search</h1>
       <div className="container">
-        <h1>Weather Search</h1>
-        <Search />
+        <Search defaultCity="Mississauga" />
       </div>
+      <p>
+        <a
+          href="https://github.com/birkanwal/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open Source Code
+        </a>{" "}
+        by Bir Kanwal
+      </p>
     </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -1,132 +1,131 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function Search() {
-  let [cityInput, setCityInput] = useState(null);
-  let [message, setMessage] = useState(null);
+export default function Search(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-  function displayWeather(response) {
+  function handleResponse(response) {
     console.log(response.data);
-    setMessage(
-      <div>
-        <h1>
-          <img
-            src={`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
-            alt={response.data.weather[0].description}
-          />
-          <span className="temperature">
-            {Math.round(response.data.main.temp)}°C
-          </span>
+    setWeatherData({
+      ready: true,
+      temperature: Math.round(response.data.main.temp),
+      description: response.data.weather[0].description,
+      feelsLike: Math.round(response.data.main.feels_like),
+      pressure: response.data.main.pressure,
+      sunrise: response.data.sys.sunrise,
+      sunset: response.data.sys.sunset,
+      humidity: response.data.main.humidity,
+      visibility: response.data.visibility / 1000,
+      iconCode: response.data.weather[0].icon,
+      country: response.data.sys.country,
+      cityName: response.data.name,
+      wind: Math.round(response.data.wind.speed * 3.6),
+      maxTemp: Math.round(response.data.main.temp_max),
+      minTemp: Math.round(response.data.main.temp_min),
+    });
+  }
 
+  function Search() {
+    const apiKey = "fc0a1c92e0473f3c314dae218cdd219d";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div>
+        <form>
+          <div className="row justify-content-around">
+            <div className="col-8">
+              <input
+                type="text"
+                placeholder="Enter City"
+                className="form-control"
+              />
+            </div>
+            <div className="col-2">
+              <button type="button" className="btn btn-primary">
+                Current
+              </button>
+            </div>
+            <div className="col-2">
+              <button type="button" className="btn btn-primary">
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
+        <div>
+          <h1>
+            <img
+              src={`http://openweathermap.org/img/wn/${weatherData.iconCode}@2x.png`}
+              alt={`weather icon`}
+            />
+            <span className="temperature">{weatherData.temperature}°C</span>
+            <br />
+            <p className="feelslike">Feels Like {weatherData.feelsLike}</p>
+            <span className="description">{weatherData.description}</span>
+          </h1>
+          <h4>
+            <span> Sunday 00:00 </span>
+          </h4>
+          <h1>
+            {weatherData.cityName}, {weatherData.country}
+          </h1>
+          <hr />
+          <div className="row current-extra">
+            <div className="col-3">
+              <strong>Pressure</strong>
+              <br />
+              <span id="pressure">10</span>
+            </div>
+            <div className="col-3">
+              <strong>Wind</strong>
+              <br />
+              <span id="wind-speed">{weatherData.speed}m/s</span>
+            </div>
+            <div className="col-3">
+              <strong>Max</strong>
+              <br />
+              <span id="max-temp">21</span>
+            </div>
+            <div className="col-3">
+              <strong>Min</strong>
+              <br />
+              <span id="min-temp">10</span>
+            </div>
+          </div>
           <br />
-          <p className="feelslike">Feels Like 15</p>
-          <span className="description">
-            {response.data.weather[0].description}
-          </span>
-        </h1>
-        <h4>
-          <span>Sunday 00:00</span>
-        </h4>
-        <h1>
-          {response.data.name}, {response.data.sys.country}
-        </h1>
-        <hr />
-        <div className="row current-extra">
-          <div className="col-3">
-            <strong>Pressure</strong>
-            <br />
-            <span id="pressure">10</span>
+          <div className="row current-extra">
+            <div className="col-3">
+              <strong>Sunrise</strong>
+              <br />
+              <span>Time</span>
+            </div>
+            <div className="col-3">
+              <strong>Sunset</strong>
+              <br />
+              <span>Time</span>
+            </div>
+            <div className="col-3">
+              <strong>Humidity</strong>
+              <br />
+              <span>{weatherData.humidity}%</span>
+            </div>
+            <div className="col-3">
+              <strong>Visibility</strong>
+              <br />
+              <span>km</span>
+            </div>
           </div>
-          <div className="col-3">
-            <strong>Wind</strong>
-            <br />
-            <span id="wind-speed">{response.data.wind.speed}m/s</span>
-          </div>
-          <div className="col-3">
-            <strong>Max</strong>
-            <br />
-            <span id="max-temp">21</span>
-          </div>
-          <div className="col-3">
-            <strong>Min</strong>
-            <br />
-            <span id="min-temp">10</span>
-          </div>
+          <hr />
         </div>
-        <br />
-        <div className="row current-extra">
-          <div className="col-3">
-            <strong>Sunrise</strong>
-            <br />
-            <span>Time</span>
-          </div>
-          <div className="col-3">
-            <strong>Sunset</strong>
-            <br />
-            <span>Time</span>
-          </div>
-          <div className="col-3">
-            <strong>Humidity</strong>
-            <br />
-            <span>{response.data.main.humidity}%</span>
-          </div>
-          <div className="col-3">
-            <strong>Visibility</strong>
-            <br />
-            <span>km</span>
-          </div>
-        </div>
-        <hr />
       </div>
     );
+  } else {
+    Search();
+
+    return "Loading";
   }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=fc0a1c92e0473f3c314dae218cdd219d&units=metric`;
-    axios.get(apiUrl).then(displayWeather);
-  }
-
-  function updateCity(event) {
-    setCityInput(event.target.value);
-  }
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div className="row justify-content-around">
-          <div className="col-8">
-            <input
-              type="text"
-              placeholder="Enter City"
-              onChange={updateCity}
-              className="form-control"
-            />
-          </div>
-          <div className="col-2">
-            <button type="button" className="btn btn-primary">
-              Current
-            </button>
-          </div>
-
-          <div className="col-2">
-            <button type="button" className="btn btn-primary">
-              Search
-            </button>
-          </div>
-        </div>
-      </form>
-      <div>{message}</div>
-      <p>
-        <a
-          href="https://github.com/birkanwal/weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open Source Code
-        </a>
-        by Bir Kanwal
-      </p>
-    </div>
-  );
 }
